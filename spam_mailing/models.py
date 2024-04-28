6132ec50d24d18bd67607817f1e8c3b22bb768d1
gremv <gremvanek@gmail.com>
2024-04-28 23:58:39 +0500

    Создание необходимых сущностей.

--- spam_mailing/models.py
+++ spam_mailing/models.py
@@ -24,3 +24,34 @@ class Client(models.Model):
         self.is_active = False
         self.save()
 
+
+class Mailing(models.Model):
+    client = models.ManyToManyField(Client, verbose_name="Клиенты")
+    send_time = models.DateTimeField(verbose_name="Время отправки")
+
+    frequency_choices = [
+        ('daily', 'Раз в день'),
+        ('weekly', 'Раз в неделю'),
+        ('monthly', 'Раз в месяц'),
+    ]
+    frequency = models.CharField(max_length=10, choices=frequency_choices, verbose_name="Рассылка")
+
+    status_choices = [
+        ('created', 'Создана'),
+        ('started', 'Запущена'),
+        ('completed', 'Завершена'),
+    ]
+    status = models.CharField(max_length=10, choices=status_choices, verbose_name="Статус")
+
+
+class Message(models.Model):
+    mailing = models.ForeignKey(Mailing, on_delete=models.CASCADE, verbose_name="Рассылка")
+    subject = models.CharField(max_length=255, verbose_name="Тема письма")
+    body = models.TextField(verbose_name="Содержание письма")
+
+
+class MailingLog(models.Model):
+    mailing = models.ForeignKey(Mailing, on_delete=models.CASCADE, verbose_name="Рассылка")
+    timestamp = models.DateTimeField(auto_now_add=True, verbose_name="Дата и время последней попытки")
+    status = models.CharField(max_length=10, verbose_name="Статус попытки")
+    server_response = models.TextField(verbose_name="Ответ почтового сервера")
