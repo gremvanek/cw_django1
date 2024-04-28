79d35a1014fd30ccc39ee261e07a08dbfee7b8ce
gremv <gremvanek@gmail.com>
2024-04-29 00:02:46 +0500

    Создание форм.

--- spam_mailing/forms.py
+++ spam_mailing/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Client
+
+from .models import Client, Mailing, Message
 
 
 class ClientForm(forms.ModelForm):
@@ -15,4 +16,38 @@ class ClientForm(forms.ModelForm):
         }
         widgets = {
             'comment': forms.Textarea(attrs={'rows': 4}),
+        }
+
+
+class MailingForm(forms.ModelForm):
+    class Meta:
+        model = Mailing
+        fields = ['client', 'send_time', 'frequency', 'status']
+        labels = {
+            'client': 'Клиенты',
+            'send_time': 'Время отправки',
+            'frequency': 'Рассылка',
+            'status': 'Статус',
+        }
+        widgets = {
+            'client': forms.SelectMultiple(attrs={'class': 'form-control'}),
+            'send_time': forms.DateTimeInput(attrs={'class': 'form-control'}),
+            'frequency': forms.Select(attrs={'class': 'form-control'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+        }
+
+
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = ['mailing', 'subject', 'body']
+        labels = {
+            'mailing': 'Рассылка',
+            'subject': 'Тема письма',
+            'body': 'Содержание письма',
+        }
+        widgets = {
+            'mailing': forms.Select(attrs={'class': 'form-control'}),
+            'subject': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
         }
