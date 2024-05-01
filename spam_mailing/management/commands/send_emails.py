269241d7b9df91cc22f8431e74d8efb58d2c4ee1
gremvanek <gremvanek@gmail.com>
2024-05-01 11:23:36 +0500

    Создание кастомной команды.

---
+++ spam_mailing/management/commands/send_emails.py
@@ -0,0 +1,14 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from django.core.management import BaseCommand
+from spam_mailing.tasks import start_scheduler
+
+
+class Command(BaseCommand):
+    help = 'Старт рассылки сообщений...'
+
+    def add_arguments(self, parser):
+        parser.add_argument('mailing_id', type=int, help='ID рассылки для запуска команды')
+
+    def handle(self, *args, **options):
+        mailing_id = options['mailing_id']
+        start_scheduler(mailing_id)
