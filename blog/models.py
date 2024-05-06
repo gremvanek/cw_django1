9be6a221e1a95d12b9efb31809170adddc6b2bb1
gremvanek <gremvanek@gmail.com>
2024-05-06 21:18:27 +0500

    Правки блога. Правки html.

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ NULLABLE = {'blank': True, 'null': True}
 class Blog(models.Model):
     title = models.CharField(max_length=250, verbose_name='Заголовок')
     body = models.TextField(verbose_name='Содержимое')
-    preview = models.ImageField(upload_to='blogs/', **NULLABLE, verbose_name='Изображение')
+    preview = models.ImageField(upload_to='static/images/', **NULLABLE, verbose_name='Изображение')
     date_is_published = models.DateField(**NULLABLE, verbose_name='Дата публикации')
     views_count = models.IntegerField(default=0, verbose_name='Просмотры')
 
